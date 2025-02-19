aa9ca6c61b40e89acf2e587d8f3c81526d3882cd
PedroPimenta-Futuro-Big-Data <pedrofpimenta16@gmail.com>
2025-02-19 17:08:08 -0300

    Update capturaPontoTela.py
    Teste do locateOnScreen para verificar se o Pyautogui está reconhecendo a imagem selecionada

--- capturaPontoTela.py
+++ capturaPontoTela.py
@@ -1,10 +1,3 @@
 import pyautogui
-
-print("Posicione o mouse sobre o local desejado e pressione Ctrl + C para parar.")
-
-try:
-    while True:
-        x, y = pyautogui.position()
-        print(f"Coordenadas: X={x}, Y={y}", end="\r")  # Atualiza na mesma linha do terminal
-except KeyboardInterrupt:
-    print("\nCaptura encerrada.")
+pos = pyautogui.locateOnScreen('assets/lupa.png', confidence=0.7)
+print("Posição encontrada:", pos)
